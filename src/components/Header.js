d6d1b76ed31e21f7e3566293a47e41719e238164
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-08-14 13:10:30 -0400

    feat:Implementación de header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="header__logo" src="" alt="" />
+      <input className="header__admin" type="button" value="admin" />
+    </div>
+  );
+}
+
+export default Header
